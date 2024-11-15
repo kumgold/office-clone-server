a18f6ced21bb422e75827ee9c8d2e3457a92e1fa
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-11-15 21:04:20 +0900

    init project

---
+++ src/main/java/com/example/officeclone/OfficeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.officeclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/officeclone/OfficeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.officeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
